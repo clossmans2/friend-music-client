47823422bd111ac758e4677f793a58ab1c3c8024
Seth Clossman <clossmans2@gmail.com>
2022-09-02 15:53:53 -0400

    Fixed the guard reference

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { MsalGuard } from '@azure/msal-angular/msal.guard';
+import { MsalGuard } from '@azure/msal-angular';
 
 import { IndexComponent } from './music/index/index.component';
 
