c6d36e96a4eed67786dbc99ccafac579748aaf0b
Seth Clossman <clossmans2@gmail.com>
2022-09-02 16:05:05 -0400

    updated authority

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,17 +4,13 @@ import { ReactiveFormsModule, FormsModule } from '@angular/forms';
 import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 // import { HttpClientInMemoryWebApiModule } from 'angular-in-memory-web-api';
 // import { InMemoryDataService } from './in-memory-data.service';
-import { MsalInterceptor, MsalModule, MSAL_BROADCAST_CONFIG, MsalService, MsalBroadcastConfiguration, MSAL_INTERCEPTOR_CONFIG, MsalGuard } from '@azure/msal-angular';
+import { MsalInterceptor, MsalModule, MsalGuard } from '@azure/msal-angular';
 import { Configuration } from 'msal';
 import { msalConfig, guardConfig, isIE, b2cPolicies, interceptConfig } from './app-config';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MusicModule } from './music/music.module';
 import { PublicClientApplication } from '@azure/msal-browser';
-
-function MSALConfigFactory(): Configuration {
-  return msalConfig;
-}
 
 @NgModule({
   declarations: [
@@ -26,7 +22,7 @@ function MSALConfigFactory(): Configuration {
     MsalModule.forRoot(new PublicClientApplication({
       auth: {
         clientId: "9a4cc7d0-25f1-402c-945f-65f876d69c7a",
-        authority: b2cPolicies.authorities.signUpSignIn.authority,
+        authority: "https://login.microsoftonline.com/common/",
         redirectUri: "https://friendmusic.clossman.co",
         postLogoutRedirectUri: "https://friendmusic.clossman.co",
         navigateToLoginRequestUrl: true,
